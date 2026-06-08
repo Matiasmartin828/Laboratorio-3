fdb3bbee06115fa7d899e40cb3deddd362d05bbb
Matías Martin <matiasmartin828@gmail.com>
2026-06-08 17:59:19 -0300

    Archivo base

---
+++ Laboratorio-3.c
@@ -0,0 +1,10 @@
+//LABORATORIO 3
+//AUTORES MATÍAS MARTIN E IVO DI MARCO
+//LABORATORIO 3
+#include <stdio.h>
+#include <stdlib.h>	
+
+int main(int argc, char *argv[]){
+    printf("Hola Mundo\n");
+    return 0;
+}
